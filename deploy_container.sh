ee90bb062d123fd54c9a205cc6110611227889c1
Marc Suchard <msuchard@gmail.com>
2015-12-03 10:20:17 -0500

    added deploy tool

---
+++ deploy_container.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -o errexit -o nounset
+addToDrat(){
+  PKG_REPO=$PWD
+
+  cd ..; mkdir drat; cd drat
+
+  ## Set up Repo parameters
+  git init
+  git config user.name "Marc A. Suchard"
+  git config user.email "msuchard@ucla.edu"
+  git config --global push.default simple
+
+  ## Get drat repo
+  git remote add upstream "https://$GH_TOKEN@github.com/OHDSI/drat.git"
+  git fetch upstream 2>err.txt
+  git checkout gh-pages
+
+  ## Install drat
+  mkdir -p ~/Rlib
+  echo 'R_LIBS=~/Rlib' > .Renviron
+  Rscript -e 'if(!"drat" %in% rownames(installed.packages())) { install.packages("drat", dependencies = TRUE) }'
+
+  ## Deploy
+  Rscript -e "drat::insertPackage('$PKG_REPO/$PKG_TARBALL', \
+    repodir = '.', \
+    commit='Travis update: build $TRAVIS_BUILD_NUMBER')"
+  git push
+
+}
+addToDrat
