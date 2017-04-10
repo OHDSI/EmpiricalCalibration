97ff63d879b1c1fc2b88f3f7eb78cecb52bb7c38
Marc Suchard <msuchard@gmail.com>
2017-04-10 13:18:19 -0700

    strange corner case

--- R/EmpiricalCalibrationUsingAsymptotics.R
+++ R/EmpiricalCalibrationUsingAsymptotics.R
@@ -3,13 +3,13 @@
 # Copyright 2015 Observational Health Data Sciences and Informatics
 #
 # This file is part of EmpiricalCalibration
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -76,7 +76,7 @@ fitNull <- function(logRr, seLogRr) {
     for (i in 1:length(estimate)) {
       result <- result - log(gaussianProduct(estimate[i], theta[1], se[i], exp(theta[2])))
     }
-    if (is.infinite(result))
+    if (length(result) == 0 || is.infinite(result))
       result <- 99999
     result
   }
